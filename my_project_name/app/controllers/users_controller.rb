12b93da7b7d1b9a75dba8ec73e10c576ba6e371e
DOV3D <davidovelazquez3@gmail.com>
2021-09-15 17:33:25 -0400

    Add User model, table, index, show, create method

--- my_project_name/app/controllers/users_controller.rb
+++ my_project_name/app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
     def index
-        render plain: "I am in the index action"
+        @users = User.all
+        render json: @users
+    end
+
+    def show
+        render json: params
     end
 
     def create
+        user = User.new(params.require(:user).permit(:name, :email))
+        user.save!
+        render json: user
     end
 end
