11da569938f196f085165ad89b9ae054e0be7621
Matthew Swedin <matthew.swedin@gmail.comgit config --global user.email matthew.swedin@gmail.com>
2021-09-16 09:51:44 -0400

    show, destroy, update

--- my_project_name/app/controllers/users_controller.rb
+++ my_project_name/app/controllers/users_controller.rb
@@ -1,16 +1,40 @@
 class UsersController < ApplicationController
     def index
-        @users = User.all
-        render json: @users
+        users = User.all
+        render json: users
     end
 
     def show
-        render json: params
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    def update
+        user = User.find(params[:id])
+        if user.update(user_params)
+            redirect_to users_url(user)
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def create
-        user = User.new(params.require(:user).permit(:name, :email))
-        user.save!
-        render json: user
+        user = User.new(user_params)
+        if user.save
+            render json: user
+        else
+            render json: users.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        redirect_to users_url
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:name, :email)
     end
 end
