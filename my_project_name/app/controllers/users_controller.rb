86225191c5fcfefa5ddfd6bdfe16e5eba15b6223
DOV3D <davidovelazquez3@gmail.com>
2021-09-16 11:13:36 -0400

    bugfix

--- my_project_name/app/controllers/users_controller.rb
+++ my_project_name/app/controllers/users_controller.rb
@@ -18,13 +18,13 @@ class UsersController < ApplicationController
         end
     end
 
-    
+
     def create
         user = User.new(user_params)
         if user.save
             render json: user
         else
-            render json: users.errors.full_messages, status: :unprocessable_entity
+            render json: user.errors.full_messages, status: :unprocessable_entity
         end
     end
 
